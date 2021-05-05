a980f3fa23b19f753b65e4039bcc29c75516d3cd
Harish Akula <harish.akula32k@gmail.com>
2021-05-05 21:11:57 +0530

    added get methods to nearest slots and postal zones

--- resources/get_nearest_slots.py
+++ resources/get_nearest_slots.py
@@ -36,3 +36,35 @@ class GetNearestSlots(Resource):
             return {
                 "message" : "There was an error accessing the database." + str(e)
             }, 500
+
+
+    #@jwt_required    
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('latitude', type=float, required=True, help="latitude cannot be left blank!")
+        parser.add_argument('longitude', type=float, required=True, help="longitude cannot be left blank!")
+        parser.add_argument('vehicle', type=int, required=True, help="did cannot be left blank!")
+        parser.add_argument('locality', type=int, required=True, help="did cannot be left blank!")
+        data = parser.parse_args()
+        #create query string
+        qstr = f""" 
+        SELECT
+        park_id, detail, available, (
+            6371 * acos (
+            cos ( radians({ data["latitude"] }) )
+            * cos( radians( latitude ) )
+            * cos( radians(longitude) - radians({ data["longitude"] }) )
+            + sin ( radians({ data["latitude"] }) )
+            * sin( radians( latitude ) )
+            )
+        ) AS distance
+        FROM parking
+        WHERE locality = '{ data["locality"] }' AND vehicle_type = '{ data["vehicle"] }'
+        ORDER BY distance;
+        """
+        try:
+            return query(qstr)
+        except Exception as e:
+            return {
+                "message" : "There was an error accessing the database." + str(e)
+            }, 500
--- resources/get_postal_zones.py
+++ resources/get_postal_zones.py
@@ -21,3 +21,19 @@ class GetPostalZones(Resource):
             return {
                 "message" : "There was an error updating the database." + str(e)
             }, 500
+
+#@jwt_required    
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('did', type=int, required=True, help="did cannot be left blank!")
+        data = parser.parse_args()
+        #create query string
+        qstr = f""" 
+        select * from postal_zones where did = "{ data['did'] }";
+        """
+        try:
+            return query(qstr)
+        except Exception as e:
+            return {
+                "message" : "There was an error updating the database." + str(e)
+            }, 500
