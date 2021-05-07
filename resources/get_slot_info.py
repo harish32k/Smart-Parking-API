bb3d6caa20d878d7ad49197f292d84cf24783509
Harish Akula <harish.akula32k@gmail.com>
2021-05-07 17:39:41 +0530

    added debug statements to get slot info

--- resources/get_slot_info.py
+++ resources/get_slot_info.py
@@ -9,17 +9,20 @@ class GetSlotInfo(Resource):
     #@jwt_required    
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('park_id', type=float, required=True, help="park_id cannot be left blank!")
+        parser.add_argument('park_id', type=int, required=True, help="park_id cannot be left blank!")
         data = parser.parse_args()
         #create query string
+        print(data)
         qstr = f""" 
         SELECT * FROM parking
         WHERE park_id = '{ data["park_id"] }'
         LIMIT 1;
         """
+        print(qstr)
         try:
             return query(qstr, json_array=False)
         except Exception as e:
+            print(str(e))
             return {
                 "message" : "There was an error accessing the database." + str(e)
             }, 500
