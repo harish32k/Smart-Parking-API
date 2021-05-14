75803eb9f848f6921cadf886f42f7340b80c3013
Harish Akula <harish.akula32k@gmail.com>
2021-05-14 12:58:21 +0530

    changed user history query

--- resources/user_history.py
+++ resources/user_history.py
@@ -13,13 +13,25 @@ class UserHistory(Resource):
         data = parser.parse_args()
         #create query string
         qstr = f""" 
-        select history.park_id, vehicle_type, detail, accessed
-        from history join parking on (history.park_id = parking.park_id) 
-        where uid = "{ data['uid'] }" order by accessed desc;
+        select history.park_id, vehicle_type, detail, accessed,
+        localities.locality, 
+        postal_zones.pincode, zone, 
+        dname 
+        from history join parking join localities join postal_zones join districts
+        on 
+        history.park_id = parking.park_id and
+        parking.locality = localities.lid and 
+        localities.pincode = postal_zones.pincode and
+        postal_zones.did = districts.did
+        where uid = "{data['uid']}" order by accessed desc;
         """
         try:
             return query(qstr)
         except Exception as e:
             return {
-                "message" : "There was an error updating the database." + str(e)
+                "message" : "There was an error accessing the database." + str(e)
             }, 500
+
+qstr1 = """select history.park_id, vehicle_type, detail, accessed
+from history join parking on (history.park_id = parking.park_id) 
+where uid = "{ data['uid'] }" order by accessed desc;"""
