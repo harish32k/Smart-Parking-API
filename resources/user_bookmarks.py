77ad1be9accef57294a392bb8e3091c9c0e40f9f
Harish Akula <harish.akula32k@gmail.com>
2021-05-14 20:16:02 +0530

    changed user_bookmarks to return required fields

--- resources/user_bookmarks.py
+++ resources/user_bookmarks.py
@@ -13,12 +13,29 @@ class UserBookmarks(Resource):
         data = parser.parse_args()
         #create query string
         qstr = f""" 
-        select bookmarks.park_id, vehicle_type, detail, bookmarked
-        from bookmarks join parking on (bookmarks.park_id = parking.park_id) 
-        where uid = "{ data['uid'] }" order by bookmarked desc;"""
+        select bookmarks.park_id, vehicle_type, detail, bookmarked,
+        available,
+        localities.locality, 
+        postal_zones.pincode, zone, 
+        dname 
+        from bookmarks join parking join localities join postal_zones join districts
+        on 
+        bookmarks.park_id = parking.park_id and
+        parking.locality = localities.lid and 
+        localities.pincode = postal_zones.pincode and
+        postal_zones.did = districts.did
+        where uid = "{data['uid']}" order by bookmarked desc;"""
+
+        print(qstr)
         try:
             return query(qstr)
         except Exception as e:
             return {
                 "message" : "There was an error updating the database." + str(e)
             }, 500
+
+
+qstr1 = """ 
+        select bookmarks.park_id, vehicle_type, detail, bookmarked
+        from bookmarks join parking on (bookmarks.park_id = parking.park_id) 
+        where uid = "{ data['uid'] }" order by bookmarked desc;"""
